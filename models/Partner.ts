45a04b19f7ab5f27149d5b3b8f3fa167f18e9597
jgsneves <jgsn.adv@gmail.com>
2022-12-21 18:07:01 -0300

    removing useless modules

--- models/Partner.ts
+++
@@ -1,5 +0,0 @@
-import { Person } from "./Person";
-
-export interface Partner extends Person {
-  cotaSocietaria: number;
-}
--- models/Person.ts
+++
@@ -1,5 +0,0 @@
-export interface Person {
-  nome: string;
-  cpf: string;
-  dataNascimento: string;
-}
